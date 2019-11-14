8a6453b6fbe303f91d830bd4a721dbff2087475d
Judith Greif <obermaedchen@gmail.com>
2019-11-14 17:26:24 +0100

    Template for unit tests

--- tests/test_discovergy.py
+++ tests/test_discovergy.py
@@ -2,5 +2,15 @@ import unittest
 from discovergy.discovergy import Discovergy
 
 
+class DiscovergyTestCase(unittest.TestCase):
+    """ Unit tests for class Discovergy """
+
+    def setUp(self):
+        self.discovergy = Discovergy("TestClient")
+
+    def tearDown(self):
+        self.discovergy.dispose()
+
+
 if __name__ == "__main__":
     unittest.main()
