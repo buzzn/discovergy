92288aec15d903eece37e9b9a82c00f277b786de
Judith Greif <obermaedchen@gmail.com>
2019-11-15 12:26:42 +0100

    Add test function for Discovergy:_fetch_consumer_tokens()

--- tests/test_discovergy.py
+++ tests/test_discovergy.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from discovergy.discovergy import Discovergy
 
 
@@ -8,34 +9,39 @@ class DiscovergyTestCase(unittest.TestCase):
     def setUp(self):
         """ Setup test suite. """
 
-        self.discovergy = Discovergy("TestClient")
-
     def test_init(self):
         """ Check function __init__() of class Discovergy. """
 
-        self.assertEqual(self.discovergy._client_name, "TestClient")
-        self.assertEqual(self.discovergy._email, "")
-        self.assertEqual(self.discovergy._password, "")
-        self.assertEqual(self.discovergy._consumer_key, "")
-        self.assertEqual(self.discovergy._consumer_secret, "")
-        self.assertEqual(self.discovergy._discovergy_oauth, None)
-        self.assertEqual(self.discovergy._base_url,
+        discovergy = Discovergy("TestClient")
+        self.assertEqual(discovergy._client_name, "TestClient")
+        self.assertEqual(discovergy._email, "")
+        self.assertEqual(discovergy._password, "")
+        self.assertEqual(discovergy._consumer_key, "")
+        self.assertEqual(discovergy._consumer_secret, "")
+        self.assertEqual(discovergy._discovergy_oauth, None)
+        self.assertEqual(discovergy._base_url,
                          'https://api.discovergy.com/public/v1')
-        self.assertEqual(self.discovergy._consumer_token_url,
-                         self.discovergy._base_url + '/oauth1/consumer_token')
-        self.assertEqual(self.discovergy._request_token_url,
-                         self.discovergy._base_url + '/oauth1/request_token')
-        self.assertEqual(self.discovergy._authorization_base_url,
-                         self.discovergy._base_url + '/oauth1/authorize')
-        self.assertEqual(self.discovergy._access_token_url,
-                         self.discovergy._base_url + '/oauth1/access_token')
-        self.assertEqual(self.discovergy._oauth_key, None)
-        self.assertEqual(self.discovergy._oauth_secret, None)
+        self.assertEqual(discovergy._consumer_token_url,
+                         discovergy._base_url + '/oauth1/consumer_token')
+        self.assertEqual(discovergy._request_token_url,
+                         discovergy._base_url + '/oauth1/request_token')
+        self.assertEqual(discovergy._authorization_base_url,
+                         discovergy._base_url + '/oauth1/authorize')
+        self.assertEqual(discovergy._access_token_url,
+                         discovergy._base_url + '/oauth1/access_token')
+        self.assertEqual(discovergy._oauth_key, None)
+        self.assertEqual(discovergy._oauth_secret, None)
 
     def test_fetch_consumer_tokens(self):
         """ Check function _fetch_consumer_token() of class Discovergy. """
 
-        print(self.discovergy._fetch_consumer_tokens())
+        discovergy = Discovergy("TestClient")
+        response = discovergy._fetch_consumer_tokens()
+
+        self.assertTrue(isinstance(response, requests.models.Response))
+        self.assertTrue(isinstance(response.content, bytes))
+        self.assertEqual(discovergy._oauth_key, response.json()['key'])
+        self.assertEqual(discovergy._oauth_secret, response.json()['secret'])
 
     def tearDown(self):
         """ Tear down test suite. """
