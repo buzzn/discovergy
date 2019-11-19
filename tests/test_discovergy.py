2c7a9706cfd612198e0eed602d638b3e81863ecf
Judith Greif <obermaedchen@gmail.com>
2019-11-19 12:41:31 +0100

    String and dict constants, use local objects in test functions

--- tests/test_discovergy.py
+++ tests/test_discovergy.py
@@ -5,6 +5,16 @@ from requests_oauthlib import OAuth1Session
 from discovergy.discovergy import Discovergy
 
 
+MOCK_RESPONSE_POST = '{"key":"9srhl1op4jemrcpafqpr2hhcq9",\
+        "secret":"i5nu32jmjsjunttq0lpi4r2qo0","owner":"TestClient",\
+        "attributes":{},"principal":null}'
+MOCK_RESPONSE_GET = 'oauth_verifier=3bfea9ada8c144afb81b5992b992303e'
+MOCK_REQUEST_TOKEN = dict(oauth_token='a866629d5ae1492d87775d6ce41b30cc',
+                          oauth_token_secret='13fb12af6acb49c3afda5406bcf44ba5', oauth_callback_confirmed='true')
+MOCK_ACCESS_TOKEN = dict(oauth_token='2a28117b269e4f99893e9f758136becc',
+                         oauth_token_secret='b75c7fc5142842afb3fd6686cacb675b')
+
+
 class MockResponse:
     """ Mock class requests.models.Response for unit testing"""
 
@@ -20,62 +30,55 @@ def mock_requests_post(*args, **kwargs):
     """ Mock function requests.post() for
     Discovergy:_fetch_consumer_tokens(). """
 
-    return MockResponse(
-        b'{"key":"9srhl1op4jemrcpafqpr2hhcq9","secret":"i5nu32jmjsjunttq0lpi4r2qo0","owner":"TestClient","attributes":{},"principal":null}', 200)
+    return MockResponse(MOCK_RESPONSE_POST.encode(), 200)
 
 
 def mock_requests_get(*args, **kwargs):
     """ Mock function requests.response() for
     Discovergy:_authorize_request_token(). """
 
-    return MockResponse(b'oauth_verifier=3bfea9ada8c144afb81b5992b992303e', 202)
+    return MockResponse(MOCK_RESPONSE_GET.encode(), 202)
 
 
 def mock_oauth1session_fetch_request_token(*args, **kwargs):
     """ Mock function OAuth1Session.fetch_request_token() for
     Discovergy:_fetch_request_token(). """
 
-    return dict(oauth_token='a866629d5ae1492d87775d6ce41b30cc',
-                oauth_token_secret='13fb12af6acb49c3afda5406bcf44ba5',
-                oauth_callback_confirmed='true')
+    return MOCK_REQUEST_TOKEN
 
 
 def mock_oauth1session_fetch_access_token(*args, **kwargs):
     """ Mock function OAuth1Session.fetch_access_token() for
     Discovergy:_fetch_access_token(). """
 
-    return dict(oauth_token='2a28117b269e4f99893e9f758136becc',
-                oauth_token_secret='b75c7fc5142842afb3fd6686cacb675b')
+    return MOCK_ACCESS_TOKEN
 
 
 class DiscovergyTestCase(unittest.TestCase):
     """ Unit tests for class Discovergy. """
 
-    def setUp(self):
-        """ Setup test suite. """
-        self.d = Discovergy("TestClient")
-
     def test_init(self):
         """ Test function __init__() of class Discovergy. """
 
-        self.assertEqual(self.d._client_name, "TestClient")
-        self.assertEqual(self.d._email, "")
-        self.assertEqual(self.d._password, "")
-        self.assertEqual(self.d._consumer_key, "")
-        self.assertEqual(self.d._consumer_secret, "")
-        self.assertEqual(self.d._discovergy_oauth, None)
-        self.assertEqual(self.d._base_url,
+        d = Discovergy('TestClient')
+        self.assertEqual(d._client_name, "TestClient")
+        self.assertEqual(d._email, "")
+        self.assertEqual(d._password, "")
+        self.assertEqual(d._consumer_key, "")
+        self.assertEqual(d._consumer_secret, "")
+        self.assertEqual(d._discovergy_oauth, None)
+        self.assertEqual(d._base_url,
                          'https://api.discovergy.com/public/v1')
-        self.assertEqual(self.d._consumer_token_url,
-                         self.d._base_url + '/oauth1/consumer_token')
-        self.assertEqual(self.d._request_token_url,
-                         self.d._base_url + '/oauth1/request_token')
-        self.assertEqual(self.d._authorization_base_url,
-                         self.d._base_url + '/oauth1/authorize')
-        self.assertEqual(self.d._access_token_url,
-                         self.d._base_url + '/oauth1/access_token')
-        self.assertEqual(self.d._oauth_key, None)
-        self.assertEqual(self.d._oauth_secret, None)
+        self.assertEqual(d._consumer_token_url,
+                         d._base_url + '/oauth1/consumer_token')
+        self.assertEqual(d._request_token_url,
+                         d._base_url + '/oauth1/request_token')
+        self.assertEqual(d._authorization_base_url,
+                         d._base_url + '/oauth1/authorize')
+        self.assertEqual(d._access_token_url,
+                         d._base_url + '/oauth1/access_token')
+        self.assertEqual(d._oauth_key, None)
+        self.assertEqual(d._oauth_secret, None)
 
     @mock.patch('requests.post', side_effect=mock_requests_post)
     def test_fetch_consumer_tokens(self, mock_post):
@@ -99,14 +102,15 @@ class DiscovergyTestCase(unittest.TestCase):
     def test_fetch_request_token(self, mock_post, mock_fetch_request_token):
         """ Test function _fetch_request_token() of class Discovergy. """
 
-        reponse = self.d._fetch_consumer_tokens()
-        request_token_oauth = OAuth1Session(self.d._oauth_key,
-                                            client_secret=self.d._oauth_secret,
+        d = Discovergy('TestClient')
+        reponse = d._fetch_consumer_tokens()
+        request_token_oauth = OAuth1Session(d._oauth_key,
+                                            client_secret=d._oauth_secret,
                                             callback_uri='oob')
 
         # Open OAuth1Session for _fetch_request_token()
         oauth_token_response = request_token_oauth.fetch_request_token(
-            self.d._request_token_url)
+            d._request_token_url)
 
         # Close OAuth1Session, otherwise it will generate a warning
         request_token_oauth.close()
@@ -131,8 +135,9 @@ class DiscovergyTestCase(unittest.TestCase):
     def test_authorize_request_token(self, mock_get):
         """ Test function _authorize_request_token() of class Discovergy. """
 
-        verifier = self.d._authorize_request_token('test@test.com', '123test',
-                                                   '719095064cbc476680700ec5bf274453')
+        d = Discovergy('TestClient')
+        verifier = d._authorize_request_token('test@test.com', '123test',
+                                              '719095064cbc476680700ec5bf274453')
 
         # Check verifier type
         self.assertTrue(isinstance(verifier, str))
@@ -146,9 +151,10 @@ class DiscovergyTestCase(unittest.TestCase):
     def test_fetch_access_token(self, mock_get, mock_fetch_access_token):
         """ Test function _fetch_access_token() of class Discovergy. """
 
-        verifier = self.d._authorize_request_token('test@test.com', '123test',
-                                                   '719095064cbc476680700ec5bf274453')
-        access_token = self.d._fetch_access_token(
+        d = Discovergy('TestClient')
+        verifier = d._authorize_request_token('test@test.com', '123test',
+                                              '719095064cbc476680700ec5bf274453')
+        access_token = d._fetch_access_token(
             '719095064cbc476680700ec5bf274453', '1f51232ace6a403a9bd2cdfff8d63a28', verifier)
 
         # Check result type
