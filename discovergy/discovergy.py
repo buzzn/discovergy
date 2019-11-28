e8d3df6a8db1c7f2f50c118717eb1eedc809fe1e
Judith Greif <obermaedchen@gmail.com>
2019-11-28 15:04:51 +0100

    Revert to 8e370f5

--- discovergy/discovergy.py
+++ discovergy/discovergy.py
@@ -1,5 +1,4 @@
 import json
-import os
 import logging
 from urllib.parse import parse_qs
 import requests
@@ -59,8 +58,6 @@ class Discovergy:
             request_token_oauth = OAuth1Session(self._oauth_key,
                                                 client_secret=self._oauth_secret,
                                                 callback_uri='oob')
-            if os.environ('TESTING') is True:
-                request_token_oauth.close()
             oauth_token_response = request_token_oauth.fetch_request_token(
                 self._request_token_url)
             result = {"token": oauth_token_response.get('oauth_token'),
